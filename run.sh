1e7e7700714b1ebc79b23c7b6f892687d22c0ae1
Paul Krizak <paul.krizak@gmail.com>
2024-01-28 10:00:22 -0800

    Use webroot auth for cert renewals to prevent certbot from needing port 80

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 set -x
 
 # Start the certbot renewal in a daily loop
-/bin/bash -c 'while true; do sleep 86400; certbot renew; done' &
+mkdir -p /tmp/acme-challenge
+/bin/bash -c 'while true; do sleep 86400; certbot renew --webroot -w /tmp/acme-challenge; done' &
 
 # Start nginx
 exec /usr/sbin/nginx -g "daemon off;"
